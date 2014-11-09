023be392623bb7cead0cdc2a79a7265c1ebce559
cjstudio <wangcj1214@gmail.com>
2014-11-09 21:24:04 +0800

    Rails Blog update

--- ruby/rails/blog/app/controllers/articles_controller.rb
+++ ruby/rails/blog/app/controllers/articles_controller.rb
@@ -3,16 +3,20 @@ class ArticlesController < ApplicationController
         
     # end
     def create
-
         @article = Article.new(article_params)
-        @article.save
-        redirect_to @article
-
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
+
     def show
         @article = Article.find(params[:id])
     end
-     
+    # def index
+        
+    # end
     private
         def article_params
             params.require(:article).permit(:title, :content)
--- ruby/rails/blog/app/models/article.rb
+++ ruby/rails/blog/app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true, length: { minimum: 3 }
 end

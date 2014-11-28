f4462b56874b7e007e0a9d749bb0fd3bef817c25
cjstudio <wangcj1214@gmail.com>
2014-11-28 20:05:15 +0800

    Rails Depot Update TEST

--- ruby/study/test1.rb
+++ ruby/study/test1.rb
@@ -1,23 +1,37 @@
 # this is my first Ruby program
 
 
-require 'io/console'
-rows, columns = $stdin.winsize
-puts "Your screen is #{columns} wide and #{rows} tall"
-class Creature
-  def initialize(name)
-    @name = name
-  end
+# require 'io/console'
+# rows, columns = $stdin.winsize
+# puts "Your screen is #{columns} wide and #{rows} tall"
+# class Creature
+#   def initialize(name)
+#     @name = name
+#   end
   
-  def fight
-    return "Punch to the chops!"
-  end
-end
+#   def fight
+#     return "Punch to the chops!"
+#   end
+# end
 
 # Add your code below!
-class Dragon < Creature
-    def fight
-        puts "Instead of breathing fire..."
-        super
-    end
+# class Dragon < Creature
+#     def fight
+#         puts "Instead of breathing fire..."
+#         super
+#     end
+# end
+
+
+def mask(ip1, ip2)
+    num = ->(ip) {
+        n=ip.split(".")
+        puts ip
+        n[0].to_i()*0x1000000+n[1].to_i()*0x10000+n[2].to_i()*0x100+n[3].to_i() 
+    }
+    m=(/(1+)$/.match((num.call(ip1)^num.call(ip2)).to_s(2))[1])
+    nip = m.to_i(2)^0xFFFFFFFF
+    (nip/0x1000000%0x100).to_s + '.' + (nip/0x10000%0x100).to_s + '.' + (nip/0x100%0x100).to_s + '.' + (nip%0x100).to_s + '/' + m.size.to_s
 end
+
+puts mask("41.58.0.0", "41.58.255.255")

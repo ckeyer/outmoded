e310afad1557ba0ee7d8664569dba73ab3963c17
cjstudio <wangcj1214@gmail.com>
2014-12-01 18:26:14 +0800

    Update Rails Depot

--- ruby/rails/depot/app/models/product.rb
+++ ruby/rails/depot/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+
+    default_scope :order => 'title'
+    
     validates :title, :description, :image_url, :presence => true
     validates :price , :numericality => {:greater_than_or_equal_to => 0.01}
     validates :title, :uniqueness => true
@@ -6,4 +9,5 @@ class Product < ActiveRecord::Base
         :with      => %r{\.(gif|jpg|png)\z}i,
         :message     => "must be a URL for GIF, IPG or PNG images"
     }
+
 end
